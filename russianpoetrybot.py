bbd2f06d494fe587996686bd98f66aa33e3b9672
dsbatov <31651228+dsbatov@users.noreply.github.com>
2019-06-20 22:26:51 +0300

    Update russianpoetrybot.py

--- russianpoetrybot.py
+++ russianpoetrybot.py
@@ -20,7 +20,7 @@ import tqdm
 
 # Настройки прокси
 
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+# PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
 
 
